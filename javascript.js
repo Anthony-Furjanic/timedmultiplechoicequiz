c994962c2376ba6153172fd8d173d1efe02515fb
unknown <anthonyf427@hotmail.com>
2023-01-11 19:35:34 -0500

    all final changes

--- javascript.js
+++ javascript.js
@@ -1,15 +1,77 @@
+
+var score=0
+var scoreboard=document.getElementById("scoreboard")
+scoreboard.innerHTML=score
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+        }
+    }, 1000);
+}
+
+starttime = function () {
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#time');
+    startTimer(twoMinutes, display);
+};
+
+Triviabutton.addEventListener("click",starttime);
+
+
+// set of questions that the user will answer
 const questionAndAnswer = [
     {
       questions: "What does HTML stand for?",
       answers: ["How To Make Lingo", "Hyper Text Magic Language"],
-      rightans: ["Hyper Text Markup Language"],
+      rightans: ["Hyper Text Markup Language"]
     },
     {
       questions:
         "Where in HTML is the main content of the page displayed?",
       answers: ["Aside", "Header"],
-      rightans: ["Body"],
+      rightans: ["Body"]
     },
+    {
+        questions:
+          "If we want define style for an unique element, then which css selector will we use?",
+        answers: ["class", "primary"],
+        rightans: ["Id"]
+    },
+    {
+     questions:
+       "If we want to wrap a block of text around an image, which css property will we use?",
+     answers: ["flex-wrap", "wrap"],
+     rightans: ["float"]
+     },
+    {
+     questions:
+       "Which of the following is a JavaScript Data Type?",
+     answers: ["Counter", "Header"],
+     rightans: ["Boolean"]
+     },
+     {
+        questions:
+          "What is Javascript?",
+        answers: ["A script that coffee machines use to brew coffee", "A computer language that determines the general format and styling of the page"],
+        rightans: ["A language used to create interactive user interfaces"]
+      },
+      {
+        questions:
+          "Which of the following is a type of Pop up box available in JavaScript?",
+        answers: ["Screenpop", "Multiple Choice"],
+        rightans: ["Alert"]
+      },
 
   ];
   
@@ -38,17 +100,30 @@ const questionAndAnswer = [
     document.getElementById(answerelement[1]).innerHTML = randomQuestion.answers[1];
     document.getElementById(answerelement[2]).innerHTML = randomQuestion.rightans[0];
   
-    document.getElementById(answerelement[0]+"input").setAttribute("data-correct","looks like you've had one too many, try again")
-    document.getElementById(answerelement[1]+"input").setAttribute("data-correct","looks like you've had one too many, try again") 
-    document.getElementById(answerelement[2]+"input").setAttribute("data-correct","Great Job, Celebrate with a beer!")
+    document.getElementById(answerelement[0]+"input").setAttribute("data-correct","not quite, try to another answer")
+    document.getElementById(answerelement[1]+"input").setAttribute("data-correct","not quite, try to another answer") 
+    document.getElementById(answerelement[2]+"input").setAttribute("data-correct","Great Job, You're on your way to becoming a developer!")
   
+
   }
   TriviaButton2.addEventListener("click",function(){
-    
-  var myanswer=document.querySelector("[name=groupOfAnswers]:checked").getAttribute("data-correct")
-  console.log(myanswer)
+    var myanswerelement=document.querySelector("#myanswer")
+    myanswerelement.innerHTML= ""
+    myanswerelement.textContent= myanswer
+    document.querySelector("[name=groupOfAnswers]:checked").checked=false
+  
+  if(myanswerelement.textContent=="data-correct"){
+  score+=10
+  scoreboard.innerHTML=score
+  
+  }
   var myanswerelement=document.querySelector("#myanswer")
   myanswerelement.innerHTML= ""
   myanswerelement.textContent= myanswer
   document.querySelector("[name=groupOfAnswers]:checked").checked=false
+
   })
+  
+
+// Add event listener to generate button
+Triviabutton.addEventListener("click", QuestionRetrieve);
